309644a2694ff86454d5489d2f539d2927db2592
TyomaMtl <scollioudfree.fr@gmail.com>
2020-06-20 22:20:31 +0200

    feat(page): create page component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <div id="app">
-
+        <page></page>
     </div>
 </template>
 
 <script>
+import page from './components/Page'
 export default {
-    
+    components: {
+        page: page
+    }
 }
 </script>
---
+++ src/components/Page.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="page" v-on:mousedown="move" v-on:wheel="zoom"></div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            scale: 0.5 // page zoom
+        }
+    },
+    methods: {
+        move: function (event) {
+            /* eslint-disable no-console */
+
+            let x = event.clientX
+            let y = event.clientY
+
+            let el = event.target
+            let style = getComputedStyle(el)
+            
+            // initial positions
+            let posY = Number(style.top.slice(0, -2))
+            let posX = Number(style.left.slice(0, -2))
+
+            el.onmousemove = function(e) {
+                let moveX = x - e.clientX
+                let moveY = y - e.clientY
+                x = e.clientX
+                y = e.clientY
+
+                posX = posX - moveX 
+                posY = posY - moveY 
+
+                el.style.left = posX + 'px'
+                el.style.top = posY + 'px'
+            }
+
+            el.onmouseup = function() {
+                el.onmousemove = null
+            }
+        },
+        zoom: function (event) {
+            let el = event.target
+
+            if (event.deltaY > 0) {
+                console.log('up')
+                if (this.scale > 0.1) {
+                    this.scale = this.scale - 0.01
+                }
+            } 
+            
+            if (event.deltaY < 0) {
+                console.log('down')
+                this.scale = this.scale + 0.01
+            }
+            
+            console.log(this.scale)
+            
+            el.style.transform = 'scale(' + this.scale + ')'
+        }
+    }
+}
+</script>
