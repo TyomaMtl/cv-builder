a44b4d1232ec5e6dbd9d8015001c59d5643a6877
TyomaMtl <scollioudfree.fr@gmail.com>
2020-07-03 11:30:35 +0200

    feat(pdf): export to pdf

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
     <div id="app">
         <builder></builder>
-        <page></page>
+        <page id="cv"></page>
+        <button @click="exportFile" class="btn-export"><i class="ic-file-pdf"></i> Export as PDF</button>
     </div>
 </template>
 
 <script>
 import page from './components/Page'
 import builder from './components/Builder'
+import jsPDF from 'jspdf'
+import html2canvas from 'html2canvas'
+
 export default {
     components: {
         page: page,
         builder: builder
+    },
+    methods: {
+        exportFile: function () {
+            html2canvas(document.querySelector('#cv'), { scale: 4 }).then(function(canvas) {
+                var img = canvas.toDataURL("image/png")
+                var doc = new jsPDF('p', 'mm', 'a4')
+                doc.addImage(img, 'PNG', 0, 0, 211, 298)
+                doc.save('my-cv.pdf')
+            })
+        }
     }
 }
 </script>
